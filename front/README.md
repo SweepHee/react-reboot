45027efe987156dbfe1834090402ce12e768ef24
Sweep Hee <jeonsh1991@gmail.com>
2019-11-16 23:07:42 +0900

    start

---
+++ front/README.md
@@ -0,0 +1,22 @@
+# REACT NODEBIRD
+
+## BUILD SETUP
+
+### 기본셋업
+```
+
+1. npm init
+2. npm i react react-dom next
+3. npm i -D nodemon webpack
+4. npm i -D eslint
+    ㄴ .eslintrc 파일 생성
+5. npm i eslint-plugin-import eslint-plugin-react eslint-plugin-react-hooks -D
+6. npm i -g next
+
+```
+
+### 디자인 ANT DESIGN
+
+```
+1npm i antd
+```
---
+++ front/pages/signup.js
@@ -0,0 +1,89 @@
+import React, { useState, useCallback } from 'react';
+import {Form, Input, Checkbox, Button} from "antd";
+
+const Signup = () => {
+
+    const [passwordCheck, setPasswordCheck] = useState('');
+    const [term, setTerm] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [termError, setTermError] = useState(false);
+
+    // 인풋검증을 아래의 커스텀훅으로 간단화 시킬 수 있다!
+    const useInput = (initValue = null) => {
+        const [value, setter] = useState(initValue);
+        const handler = (e) => {
+            setter(e.target.value);
+        };
+        return [value, handler];
+    };
+
+    const [id, onChangeId] = useInput('');
+    const [nick, setNick] = useInput('');
+    const [password, setPassword] = useInput('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(password !== passwordCheck) {
+            return setPasswordError(true);
+        }
+        if(!term){
+            return setTermError(true);
+        }
+        console.log({
+            id,
+            nick,
+            password,
+            passwordCheck,
+            term,
+        });
+    };
+
+    const onChangePasswordCheck = (e) => {
+        setPasswordError(e.target.value !== password);
+        setPasswordCheck(e.target.value);
+    };
+    const onChangeTerm = (e) => {
+        setTermError(false);
+        setTerm(e.target.checked);
+    };
+
+
+
+    return (
+        <>
+            <Form onSubmit={onSubmit} style={{ padding: 10 }}>
+                <div>
+                    <label htmlFor="user-id">아이디</label>
+                    <br />
+                    <Input name={"user-id"} value={id} required onChange={onChangeId} />
+                </div>
+                <div>
+                    <label htmlFor="user-nick">닉네임</label>
+                    <br />
+                    <Input name={"user-nick"} value={nick} required onChange={onChangeNick} />
+                </div>
+                <div>
+                    <label htmlFor="user-password">비밀번호</label>
+                    <br />
+                    <Input name={"user-password"} value={password} type={"password"} required onChange={onChangePassword} />
+                </div>
+                <div>
+                    <label htmlFor="user-password-check">비밀번호확인</label>
+                    <br />
+                    <Input name={"user-password-check"} value={passwordCheck} type={"password"} required onChange={onChangePasswordCheck} />
+                    {passwordError && <div style={{color: 'red'}}>비밀번호가 일치하지 않습니다.</div>}
+
+                </div>
+                <div>
+                    <Checkbox name={"user-term"} value={term} onChange={onChangeTerm}>동의합니다</Checkbox>
+                    {termError && <div style={{color: 'red'}}>약관에 동의하셔야 합니다.</div>}
+                </div>
+                <div style={{ marginTop: 10 }}>
+                    <Button type={"primary"} htmlType={"submit"}>가입하기</Button>
+                </div>
+            </Form>
+        </>
+    )
+};
+
+export default Signup;
