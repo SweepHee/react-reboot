7cabd1d2714cb937572b782a927e09ffc693494e
Sweep <jeonsh1991@gmail.com>
2019-11-26 01:06:01 +0900

    api action

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,14 +1,24 @@
+import axios from 'axios';
+
 const dummyUser = {
     nickname: '스윕',
-    Post: ["1","1"],
-    Followings: ["0","1"],
-    Followers: ["0","1"],
+    Post: [],
+    Followings: [],
+    Followers: [],
 }
 
 export const initialState = {
-    isLoggedIn: false,
-    user: null,
-    signUpdata: {},
+    isLoggedIn: false, // 로그인 여부
+    isLoggingOut: false, // 로그아웃 시도중
+    isLoggingIn: false, // 로그인 시도중
+    logInErrorReason: '', // 로그인 에러 사유
+    signedUp: false, // 회원가입 성공
+    isSigningUp: false, // 회원가입 시도중
+    signUpErrorReason: '', // 회원가입 실패 사유
+    me: null, // 내 정보
+    followingList: [], // 팔로잉 리스트
+    followerList: [], // 팔로워 리스트
+    userInfo: null, //남의 정보
 };
 
 export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
@@ -19,11 +29,31 @@ export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'; 
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE'; 
 
+export const LOAD_USER_REQUEST = "LOAD_USER_REQUEST";
+export const LOAD_USER_SUCCESS = "LOAD_USER_SUCCESS";
+export const LOAD_USER_FAILURE = "LOAD_USER_FAILURE";
+
 export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
 export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
 export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
 
+export const LOAD_FOLLOW_REQUEST = "LOAD_FOLLOW_REQUEST";
+export const LOAD_FOLLOW_SUCCESS = "LOAD_FOLLOW_SUCCESS";
+export const LOAD_FOLLOW_FAILURE = "LOAD_FOLLOW_FAILURE";
 
+export const FOLLOW_USER_REQUEST = "FOLLOW_USER_REQUEST";
+export const FOLLOW_USER_SUCCESS = "FOLLOW_USER_SUCCESS";
+export const FOLLOW_USER_FAILURE = "FOLLOW_USER_FAILURE";
+
+export const UNFOLLOW_USER_REQUEST = "UNFOLLOW_USER_REQUEST";
+export const UNFOLLOW_USER_SUCCESS = "UNFOLLOW_USER_SUCCESS";
+export const UNFOLLOW_USER_FAILURE = "UNFOLLOW_USER_FAILURE";
+
+export const REMOVE_USER_REQUEST = "REMOVE_USER_REQUEST";
+export const REMOVE_USER_SUCCESS = "REMOVE_USER_SUCCESS";
+export const REMOVE_USER_FAILURE = "REMOVE_USER_FAILURE";
+
+export const ADD_POST_TO_ME = "ADD_POST_TO_ME";
 
 /* --------- ACTIONS --------- */ 
 
@@ -41,23 +71,29 @@ export const signUp = data => ({  // 소괄호로 return 을 생략 가능
 /* ----------------------------- */
 
 
-const reducer = (state = initialState, action) => {
+export default (state = initialState, action) => {
     switch (action.type)
     {
         case LOG_IN_REQUEST: {
             return {
                 ...state,
-                loginData: action.data,
                 isLoading: true,
             }
         }
         case LOG_IN_SUCCESS: {
             return {
                 ...state,
-                user: dummyUser,
+                me: dummyUser,
                 isLoggedIn: true,
                 isLoading: false,
             }
+        }
+        case LOG_IN_FAILURE: {
+            return {
+                ...state,
+                isLoggedIn: false,
+                me: null,
+            };
         }
         case LOG_OUT_REQUEST: {
             return {
@@ -80,5 +116,3 @@ const reducer = (state = initialState, action) => {
         }
     }
 };
-
-export default reducer;
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,11 +1,48 @@
-import { all, fork, takeLatest } from 'redux-saga/effects';
+import { all, fork, takeLatest, takeEvery } from 'redux-saga/effects';
 import { LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
+import axios from 'axios';
+
+/*
+ * 패턴
+~~~API() 함수 : 서버에 요청을 보내는 API 함수
+watch~~~() 함수 : 어떤 액션이 들어오면 어떤걸 하겠다 명시
+(signUp, logIn logOut 등등 함수) : watch 함수가 실행됐을때 무엇을 할 지
+
+정리하자면,
+
+액션이 들어온다(watch함수) -> watch함수에 명시된 함수가 실행됨.
+-> 명시된 함수에서 call로 서버로 axios 요청 들어감 -> 성공하면 SUCCESS 액션 실행
+
+*/
+
 
 function loginAPI() {
-    // 서버에 요청을 보내는 부분
+    return axios.post("/login");
 }
 
 function* login() {
+    try {
+        yield call(loginAPI);
+        yield put({
+            type: LOG_IN_SUCCESS
+        });
+    }catch(e) {
+        console.error(e);
+        yield put({
+            type: LOG_IN_FAILURE
+        })
+    }
+}
+
+function* watchLogin () {
+    yield takeEvery(LOG_IN_REQUEST, login)
+}
+
+function signUpAPI() {
+    return axios.post("/login");
+}
+
+function* signUp() {
     try {
         yield call(loginAPI);
         yield put({
@@ -19,12 +56,12 @@ function* login() {
     }
 }
 
-function* watchLogin () {
-    yield takeLatest(LOG_IN, login)
+function* watchSignUp() {
+    yield takeEvery(SIGN_UP_REQUEST, signUp);
 }
 
 export default function* userSaga() {
     yield all([
-        
+
     ])
 }
